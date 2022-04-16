14dbe365f4cd0512dedce49d9f273d1003086186
ariyaanshaikh <103813776+ariyaanshaikh@users.noreply.github.com>
2022-04-16 12:45:08 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+consumer_key= 'OLeIQ2eYAz8v8CVSVbXDgRu9o'
+consumer_secret= 'y8nepn02YBuZNknbNSf5Ngg2N2tzkkLSG4wckag71E8WqEs1Vq'
+
+access_token='1154076723467579393-s8z1luNOhIj46O1jSpOfNG4H2cvXCU'
+access_token_secret='Oi3W3TYam7RDbuVvuvSUr0699r64E8gMi0iCdrWHKoWjm'
+
+num_of_tweets = int(300)
